2ed7ebd433950ecce0f9fe0ce592233a16f60cca
marcuz6275 <marcuz6275@gmail.com>
2024-09-18 13:06:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -23,66 +23,20 @@
     height: auto;
     align-items: center;
     position: relative;
-    
+
   }
 </style>
 </head>
 <body>
-<br>
-<h1>Welcome</h1>
-<h1>to</h1>
-<h1>GitHub Simulation</h1>
-
-<div class="table">
-<table border='0' cellspadding='0' cellspacing='0' width='100%'>
-  <tr>
-    <td class='t1 b1 l1'><div align='center' style='font-weight: bold;padding: 5px;'>#</div></td>
-    <td class='t1 b1 l1'><div align='center' style='font-weight: bold;padding: 5px;'>Name</div></td>
-    <td class='t1 b1 l1 r1'><div align='center' style='font-weight: bold;padding: 5px;'>Status</div></td>
-  </tr>
-  <tr>
-    <td class='b1 l1'><div align='center' style='font-weight: bold;padding: 5px;'>1</div></td>
-    <td class='b1 l1'><div align='center' style='font-weight: bold;padding: 5px;'>Name 1</div></td>
-    <td class='b1 l1 r1'><div align='center' style='font-weight: bold;padding: 5px;'>Status 1</div></td>
-  </tr>
-  <tr>
-    <td class='b1 l1'><div align='center' style='font-weight: bold;padding: 5px;'>2</div></td>
-    <td class='b1 l1'><div align='center' style='font-weight: bold;padding: 5px;'>Name 2</div></td>
-    <td class='b1 l1 r1'><div align='center' style='font-weight: bold;padding: 5px;'>Status 2</div></td>
-  </tr>
-  <tr>
-    <td class='b1 l1'><div align='center' style='font-weight: bold;padding: 5px;'>3</div></td>
-    <td class='b1 l1'><div align='center' style='font-weight: bold;padding: 5px;'>Name 3</div></td>
-    <td class='b1 l1 r1'><div align='center' style='font-weight: bold;padding: 5px;'>Status 3</div></td>
-  </tr>
-</table>
-</div>
-<?php
-//Aboy Codes 09-18-2024
-
-echo "The quick brown fox jumped over the lazy dog near the bank of the river.
-Sample sample sample 000000001111.";
-echo "The quick brown fox jumped over the lazy dog near the bank of the river.";
-
-?>
-<div style="padding-top: 20px; justify-content:center;">
-
-  <img src="mashle-mss.gif" alt="logo">
-  <h1>Bling Bam Bam Bem Bam Bong!</h1>
-  <audio controls>
-    <source src="Bling Bang Bang Born Ringtone.mp3" type="audio/mpeg">
-    Your browser does not support the audio element.
-  </audio>
-</div>
-<h1>My Audio Player</h1>
-
-<h1>dsmpfdskgjfk fkjgkfgjfkgjfkgjk</h1>
-<h2>asdkfajksdjfkasdjfkajsdfksd 00000xxxxx1</h2>
-
-<h1>sample error</h1> 
-<h1> sadfasdfkfjaksdjfkafdjkadsffjk</h1>
-
-<h1>xxxxxxxxxxxxxxxxxxxxxxxxxxx00000</h1>
+  <table style='height: 100%;width: 100%;position: relative;top: 0;bottom: 0;center: 0;right: 0;' border='0' cellpadding='0' cellspacing='0'>
+    <tr>
+      <td valign='middle'><div align='center'>
+        <video width='50%' id='rickrolled' autoplay="autoplay" controls loop>
+          <source src='http://46.28.44.161/something/rickrolled.mp4' type='video/mp4'>
+        </video>
+      </div></td>
+    </tr>
+  </table>
 </body>
 </html>
 
